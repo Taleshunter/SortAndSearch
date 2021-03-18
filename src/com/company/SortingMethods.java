eca4f9bff695a0f24350c73c6407df3dd2bed0bb
Taleshunter <73743415+Taleshunter@users.noreply.github.com>
2021-03-18 09:48:43 +0100

    improved bubblesort for lists

--- src/com/company/SortingMethods.java
+++ src/com/company/SortingMethods.java
@@ -24,13 +24,20 @@ public class SortingMethods {
     }
 
     public static <T extends Comparable<T>> void BubbleSort(List<T> list){
+        boolean performedSwap;
         for (int i=0; i< list.size()+1; i++){
+            performedSwap = false;
             for (int j=0; j< list.size()-1-i; j++){
                 if (list.get(j).compareTo(list.get(j+1))>0){
                     T temp = list.get(j);
                     list.set(j, list.get(j+1));
                     list.set(j+1,temp);
+                    performedSwap=true;
                 }
+            }
+            if (!performedSwap){
+                System.out.println("Left at iteration number: " + i);
+                return;
             }
         }
 
